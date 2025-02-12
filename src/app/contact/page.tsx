880f83e92e1c83b0d501233e4151a0e0fdf3fb37
Ranjan <ranjan@pearlorganisation.com>
2025-02-12 16:05:25 +0530

    finished contactus page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,24 +1,48 @@
 'use client';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AbstractHero from "@/components/AbstractHero";
 import ContactUsSection from "../../components/ContactUsSection";
 import FormContact from "../../components/forms/FormContact";
 import { PiWechatLogoFill, PiQuotesFill } from "react-icons/pi";
 import bg_contact from "../../../public/img/bg-contact.png";
+import { backendBaseUrl } from "@/components/utils/backendUrl";
 
 
 export default function Contact() {
   const [showForm, setShowForm] = useState(false);
-    const content = (
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setIsError]= useState(false);
+  const [contactData, setContactData] = useState<any>(null);
+
+  useEffect(()=>{
+     const fetchContact = async()=>{
+          try {
+            setLoading(true);
+            setIsError(false) 
+            const res = await fetch(`${backendBaseUrl}/contact-us-page`);
+            const data = await res.json()
+            setContactData(data.data)
+          } catch (error) {
+            setIsError(true)
+            throw error
+          }finally{
+            setLoading(false)
+          }
+        }
+        fetchContact()
+  },[])
+console.log("the contact data is", contactData)
+    const content = `
     <div>
       Are you an aspiring entrepreneur, early-stage startup founder, investor, potential
       partner or mentor?
     </div>
-  );
+  `
 
   return (
     <>
-      <AbstractHero content={content} bg={bg_contact.src} />
+    <AbstractHero content={content} bg={bg_contact.src} />
+       
       <section className="min-h-[45vh] py-5 container mx-auto flex flex-col mb-20">
         <div className="py-8 px-4 mx-auto w-[80%] sm:py-16 lg:px-6 grow rounded-lg content-center bg-accent justify-center text-center ">
           <div className="max-w-4xl z-10 flex flex-col justify-center items-center mx-auto">
@@ -28,16 +52,17 @@ export default function Contact() {
           </div>
           <article className="prose mx-auto max-w-[45rem] text-2xl text-black">
           <h1 className="text-4xl font-mono font-medium">Contact Us</h1>
-          <p>
+            <p>
             Are you an aspiring entrepreneur, early-stage startup founder, investor,
             potential partner or mentor? Learn the different ways to join the Transition
             VC and connect with us!
-          </p>
+          
+          </p> 
           </article>
           <button onClick={() => setShowForm(true)} className="btn btn-secondary btn-wide btn-lg mt-8 font-mono text-black text-2xl text-nowrap">Connect with us !!!</button>
         </div>
       </section>
-      <ContactUsSection/>
+        {contactData && <ContactUsSection data={contactData} /> }  
       <FormContact isOpen={showForm} onClose={() => setShowForm(false)} />
     </>
   );
--- src/components/ContactUsSection.tsx
+++ src/components/ContactUsSection.tsx
@@ -7,16 +7,95 @@ import FormGetInTouch from "./forms/FormGetInTouch";
 import FormConversation from "./forms/FormConversation";
 import FormContact from "./forms/FormContact";
 
-const ContactUsSection = () => {
+// const dummy = [
+//   {
+//     title,
+//     fieldArr : [{
+//       title,
+//       type,
+//       formHeading,
+//     }]
+
+//   }
+// ]
+const ContactUsSection = ({data}: any) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isClickHereFormOpen, setIsClickHereFormOpen] = useState(false);
     const [isGetInTouchFormOpen, setIsGetInTouchFormOpen] = useState(false);
     const [isConversationFormOpen, setIsConversationFormOpen] = useState(false);
     const [isContactFormOpen, setIsContactFormOpen] = useState(false);
+    console.log("the contact data is in section is", data)
     
+    const handleClick = (data:any)=>{
+
+    }
   return (
     <div className="container mx-auto px-4 py-8 space-y-12 mb-20">
-      <section className="space-y-4">
+      {Array.isArray(data) && data?.map((el)=>(
+        <>
+        <section className="space-y-4">
+        <h2 className="tracking-[.6rem] text-[#5C5C5C] font-mono font-medium">
+          {el?.mainTitle}
+        </h2>
+          
+
+        <div className="text-2xl/8">
+        {el?.fieldArr?.map((item:any)=>(<>
+            {item?.title}
+            
+          
+        {item?.type === "FORM" ? (
+        <>
+           <button
+            onClick={() => setIsModalOpen(true)}
+            className="text-primary font-mono font-medium hover:underline"
+          >
+            {item?.buttonText}
+          </button><br />
+        </>
+      ) : item?.type === "LINK" ? (
+        <Link
+          href={item?.value}
+          className="text-primary font-mono font-medium hover:underline"
+        >
+          here&gt;
+        </Link>
+      ) : (
+  <a
+    href="mailto:info@transitionventurecapital.com"
+    className="text-primary font-mono font-medium underline"
+  >
+    info@transitionventurecapital.com
+  </a>
+)}
+
+
+         </>))}      
+{/*           
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="text-primary font-mono font-medium hover:underline"
+          >
+            Submit Pitch Deck&gt;
+          </button> */}
+          {/* <br />
+          Not ready to apply, but want to learn more about how Transition VC can help you?{" "}
+          <button
+            onClick={() => setIsClickHereFormOpen(true)}
+            className="text-primary font-mono font-medium hover:underline"
+          >
+            Click here&gt;
+          </button>
+          <br /> connected?{" "}
+          <button
+            onClick={() => setIsGetInTouchFormOpen(true)}
+            className="text-primary font-mono font-medium hover:underline"
+          >Get in touch&gt;
+            </button> */}
+        </div>
+      </section>
+      <div className="divider divider-success"></div></>))}
+      {/* <section className="space-y-4">
         <h2 className="tracking-[.6rem] text-[#5C5C5C] font-mono font-medium">
           FOUNDERS
         </h2>
@@ -44,8 +123,8 @@ const ContactUsSection = () => {
             </button>
         </div>
       </section>
-      <div className="divider divider-success"></div>
-      <section className="space-y-4">
+      <div className="divider divider-success"></div> */}
+      {/* <section className="space-y-4">
         <h2 className="tracking-[.6rem] text-[#5C5C5C] font-mono font-medium">INVEST</h2>
         <div className="text-2xl/8">
           Want to invest in Transition VC fund?{" "}
@@ -94,7 +173,7 @@ const ContactUsSection = () => {
             info@transitionventurecapital.com
           </a>
         </p>
-      </section>
+      </section> */}
       <FormPitchDeck isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
       <FormClickHere isOpen={isClickHereFormOpen} onClose={() => setIsClickHereFormOpen(false)} />
       <FormGetInTouch isOpen={isGetInTouchFormOpen} onClose={() => setIsGetInTouchFormOpen(false)} />
